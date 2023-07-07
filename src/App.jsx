c1163bb37994330abbaecdbab60c77ce9d592552
LTK <gift.letuka21@gmail.com>
2023-07-07 16:07:34 +0200

    Add first change

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Ding Dong ,Kuthulekile
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       DIng ding ding ding ding ding ding
       </p>
     </>
   )

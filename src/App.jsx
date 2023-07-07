ff60caeb675eeface9c580479d2da7ef8556d344
LTK <gift.letuka21@gmail.com>
2023-07-07 17:51:04 +0200

    server updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001'); // Replace with your server URL
+const socket = io('https://island-romantic-punishment.glitch.me/'); // Replace with your server URL
 
 const App = () => {
   const [cards, setCards] = useState([]);
